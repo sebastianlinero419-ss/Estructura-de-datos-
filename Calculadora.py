f16a052a2c6b50a16233fb5bec95bb1e198e71dd
sebastianlinero419-ss <sebastianlinero419@gmail.com>
2026-08-14 20:20:37 -0500

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,32 @@
+def sumar (num1, num2):
+    total = num1 + num2
+    return total
+
+def restar (num1, num2):
+    total = num1 - num2
+    return num1 - num2
+
+def multiplicar (num1, num2):
+    total = num1 * num2
+    return num1 * num2
+
+def dividir (num1, num2):
+    if (num2==0):
+        return "Nose puede hacer esta division"
+         
+    total = num1 / num2
+    return num1 / num2    
+    
+
+
+def main():
+    num1 = 5
+    num2 = 0
+
+    print (sumar(num1, num2))
+    print (restar(num1, num2))
+    print (multiplicar(num1, num2))
+    print (dividir(num1, num2))
+
+if __name__ == "__main__":
+    main()
