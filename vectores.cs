ca8a9a9e871f96054f75a0bca159c6ee4f11bc55
sebastianlinero419-ss <sebastianlinero419@gmail.com>
2026-09-18 13:14:34 -0500

    Create vectores.cs

---
+++ vectores.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5]
+                { 15, 42, 8, -4, 99};
+            Console.WriteLine($"elemento en [0]: {numeros[0]}");
+
+        }
+    }
+}
